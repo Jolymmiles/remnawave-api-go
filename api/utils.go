0c03e6e0451b83e957ff498e563f6ad9b094aace
jesus <valmak29@gmail.com>
2025-07-29 20:54:50 +0400

    feat: add 2.0.1

--- api/utils.go
+++
@@ -1,9 +0,0 @@
-package api
-
-import "context"
-
-type StaticToken struct{ Token string }
-
-func (s StaticToken) Authorization(_ context.Context, _ OperationName) (Authorization, error) {
-	return Authorization{Token: s.Token}, nil
-}
