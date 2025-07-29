3a84529c66f0101c8897598785605e1ff756b3c7
jesus <valmak29@gmail.com>
2025-07-29 20:57:57 +0400

    feat: add 2.0.1

--- generate.go
+++ generate.go
@@ -1,3 +1,3 @@
 package main
 
-//go:generate go run github.com/ogen-go/ogen/cmd/ogen@latest --config .ogen.yml --target api --package api  --clean openapi.json
+//go:generate go run github.com/ogen-go/ogen/cmd/ogen@latest --config .ogen.yml --target api --package api  --clean remnawave-api-v200.json
